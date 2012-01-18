74a2ad8153f9e5378b5829b2d23494431af1894f
vinoth <vinoth@vinoth-vm.(none)>
2012-01-18 15:00:17 -0500

    init

---
+++ logStats.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define MAX_COUNT 10
+
+struct block {
+	int sIndex;
+	int eIndex;
+} *fBlocks;
+
+int count[MAX_COUNT];
+char *psList[MAX_COUNT];
+//int *offSet;
+
+int getStat(int procId) {
+
+//Read each ps from log file
+//for each ps ,do findMatch and increment the count of index returned by it.
+	return 0;
+}
+
+int findMatch(char *s) {
+
+	int index=-1;
+
+
+	return index;
+} 
+
+
+int findBlockIndices(char *fileName, int procCount ) {
+//Fill in sIndex and eIndex of fBlocks[0..procCount-1]
+
+}
+
+
+int main() {
+
+}
